6b942117c834317ae07793464b8d70bf90f73637
François Xavier AMPE <FX.Ampe@octime.com>
2023-03-25 17:28:31 +0100

    Commit 2 appFlask

---
+++ appFlask/tests/test_sentiment.py
@@ -0,0 +1,11 @@
+import sys, os
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from app import *
+
+negative_text = "It's a bad day, I want to cry because I broke with my girlfriend. It's a poor feeling !"
+positive_text = "What a nice restaurant, sushis were very tasteful."
+def test_negative():
+    assert predict_sentiment(negative_text)[0] == "negative"
+
+def test_positive():
+    assert predict_sentiment(positive_text)[0] == "positive"
