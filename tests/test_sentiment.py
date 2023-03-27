73743308bff3ec27dc4654adef85a143e08a3112
fxmx86 <122381470+fxmx86@users.noreply.github.com>
2023-03-27 13:59:38 +0200

    Update test_sentiment.py

--- tests/test_sentiment.py
+++ tests/test_sentiment.py
@@ -1,6 +1,7 @@
 import sys, os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from app import *
+#from app import *
+from app import predict_sentiment
 
 negative_text = "It's a bad day, I want to cry because I broke with my girlfriend. It's a poor feeling !"
 positive_text = "What a nice restaurant, sushis were very tasteful."
