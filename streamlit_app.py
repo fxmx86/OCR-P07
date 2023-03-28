4c94bffecde412c8b71bb7e6916b458445ff5539
fxmx86 <122381470+fxmx86@users.noreply.github.com>
2023-03-28 17:11:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,25 +3,24 @@ import streamlit as st
 import requests
 import numpy as np
 
-while True:
-  text = st.text_input("Text input for sentiment classification API", "It was a quite bad day, poor feeling because I'm sick")
-  if text == "": break
+text = st.text_input("Text input for sentiment classification API", placeholder="It was a quite bad day, poor feeling because I'm sick")
+if text == "": break
 
-  #request_data = {'text': "This movie was badly useless, actors had a bad acting game it's a shame"}
-  #request_data = {'text': "Today is a good day because I am so in love to this guy"}
-  #request_data = {'text': "The flight was very nice and one of the stewards was very kind and handsome to me"}
-  #request_data = {'text': "What a nice restaurant, sushis were very tasteful."}
-  #request_data = {'text': "It's a bad day, I want to cry because I broke with my girlfriend. It's a poor feeling !"}
-  request_data = {'text': text}
+#request_data = {'text': "This movie was badly useless, actors had a bad acting game it's a shame"}
+#request_data = {'text': "Today is a good day because I am so in love to this guy"}
+#request_data = {'text': "The flight was very nice and one of the stewards was very kind and handsome to me"}
+#request_data = {'text': "What a nice restaurant, sushis were very tasteful."}
+#request_data = {'text': "It's a bad day, I want to cry because I broke with my girlfriend. It's a poor feeling !"}
+request_data = {'text': text}
 
-  # URL of the API
-  url = 'https://fxa-ocrp07-flaskapi.azurewebsites.net/predict_sentiment'
+# URL of the API
+url = 'https://fxa-ocrp07-flaskapi.azurewebsites.net/predict_sentiment'
 
-  st.write('Ok !')
-  st.write('Sending a request for this text :', text)
-  # Send the request containing the text to the API and grab the response r
-  # Remember r is the json dict containg the sentiment and the probability
-  r = requests.post(url, params=request_data)
+st.write('Ok !')
+st.write('Sending a request for this text :', text)
+# Send the request containing the text to the API and grab the response r
+# Remember r is the json dict containg the sentiment and the probability
+r = requests.post(url, params=request_data)
 
-  # Display the image array of the mask color image
-  st.write('API answer :', r.json())
+# Display the image array of the mask color image
+st.write('API answer :', r.json())
